7df741508da519b449be15265fed209be6b96108
blessingadomakoh <badomakoh02@gmail.com>
2023-07-08 17:17:25 -0700

    deploying backend

--- lifetracker-api/config.js
+++ lifetracker-api/config.js
@@ -1,31 +1,3 @@
-// require('dotenv').config();
-
-// const PORT = process.env.PORT || 3001;
-// const SECRET_KEY = process.env.SECRET_KEY || 'my-secret-key';
-// const BCRYPT_WORK_FACTOR = process.env.BCRYPT_WORK_FACTOR || 12;
-// const IS_TESTING = process.env.NODE_ENV === 'test';
-
-// function getDatabaseUri() {
-//   if (process.env.DATABASE_URL) {
-//     return process.env.DATABASE_URL;
-//   }
-
-//   if (IS_TESTING) {
-//     return 'lifetracker_test';
-//   }
-
-//   // Combine other environment variables to construct the database URI
-//   return `postgresql://${    process.env.DATABASE_USER}:${process.env.DATABASE_PASS
-//     }@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
-// }
-
-// module.exports = {
-//   PORT,
-//   SECRET_KEY,
-//   BCRYPT_WORK_FACTOR,
-//   IS_TESTING,
-//   getDatabaseUri,
-// };
 
 require("dotenv").config();
 require("colors");
@@ -37,7 +9,8 @@ function getDatabaseUri() {
   const dbPass = process.env.DATABASE_PASS
     ? encodeURI(process.env.DATABASE_PASS)
     : "postgres";
-  const dbHost = process.env.DATABASE_HOST || "localhost";
+  // const dbHost = process.env.DATABASE_HOST || "localhost";
+  const dbHost = process.env.DATABASE_HOST 
   const dbPort = process.env.DATABASE_PORT || 5432;
   const dbTestName = process.env.DATABASE_TEST_NAME || "lifetracker_test";
   const dbProdName = process.env.DATABASE_NAME || "lifetracker";
