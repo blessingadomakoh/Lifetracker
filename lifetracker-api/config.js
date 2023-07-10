5882bdb9f381a83ed6b6754580d1000a9496c74e
blessingadomakoh <badomakoh02@gmail.com>
2023-07-10 11:16:44 -0700

    stiilll deploying

--- lifetracker-api/config.js
+++ lifetracker-api/config.js
@@ -11,8 +11,8 @@ function getDatabaseUri() {
   const dbPass = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : "postgres"
   const dbHost = process.env.DATABASE_HOST || "local"
   const dbPort = process.env.DATABASE_PORT || 5432
-  const dbTestName = process.env.DATABASE_TEST_NAME || "vaccine_hub_test"
-  const dbProdName = process.env.DATABASE_NAME || "vaccine_hub"
+  const dbTestName = process.env.DATABASE_TEST_NAME || "lifetracker_test"
+  const dbProdName = process.env.DATABASE_NAME || "lifetracker"
   const dbName = process.env.NODE_ENV === "test" ? dbTestName : dbProdName
 
   return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
@@ -20,7 +20,7 @@ function getDatabaseUri() {
 
 const BCRYPT_WORK_FACTOR = IS_TESTING ? 1 : 13
 
-console.log("Vaccine Hub Config:".red)
+console.log("Lifetracker Config:".red)
 console.log("PORT:".blue, PORT)
 console.log("BCRYPT_WORK_FACTOR".blue, BCRYPT_WORK_FACTOR)
 console.log("Database:".blue, getDatabaseUri())
