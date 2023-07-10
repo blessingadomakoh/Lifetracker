8ebcd232311be456e50b5ab6580a29208136aead
blessingadomakoh <badomakoh02@gmail.com>
2023-07-10 10:53:36 -0700

    still deploy

--- lifetracker-ui/src/constants.js
+++ lifetracker-ui/src/constants.js
@@ -1,7 +1,12 @@
-export const PRODUCTION_API_BASE_URL = "http://localhost:5173";
+export const PRODUCTION_API_BASE_URL = "https://lifetracker-backend-d41r.onrender.com";
 export const DEVELOPMENT_API_BASE_URL = "http://localhost:3001";
 
 export const API_BASE_URL =
   process.env.NODE_ENV === "production"
     ? PRODUCTION_API_BASE_URL
     : DEVELOPMENT_API_BASE_URL;
+
+console.log(`API_BASE_URL IS :${API_BASE_URL}`);
+
+
+
